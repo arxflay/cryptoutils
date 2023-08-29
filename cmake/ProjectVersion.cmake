1d8b614eb3562de128400ddd2524cb7c1e24b0fc
arxflay <arxflay@yandex.ru>
2023-08-29 12:14:10 +0200

    Add installer and restructure cmake project

--- cmake/ProjectVersion.cmake
+++ cmake/ProjectVersion.cmake
@@ -1,4 +1,3 @@
-include(CPack)
 find_package(Git REQUIRED)
 execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} describe --abbrev=0
     RESULT_VARIABLE GIT_PROJ_TAG ERROR_QUIET)
@@ -8,9 +7,7 @@ string(REGEX MATCH "^v([0-9]+.)+[0-9]$" VERSION_TAG "${GIT_PROJ_TAG}")
 if (NOT VERSION_TAG)
     execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} rev-parse --short HEAD
         OUTPUT_VARIABLE COMMIT_ID ERROR_QUIET)
-
     string(STRIP ${COMMIT_ID} COMMIT_ID)
-    message(STATUS ${COMMIT_ID})
     execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} describe --abbrev=0 --tags
         OUTPUT_VARIABLE GIT_PROJ_TAG ERROR_QUIET)
     string(REGEX MATCH "^v([0-9]+.)+[0-9]$" VERSION_TAG "${GIT_PROJ_TAG}")
@@ -31,17 +28,5 @@ else()
 endif()
 
 if (COMMIT_ID)
-    set(PKG_VER_NAME "${PKG_VER_NAME}-(${COMMIT_ID})")
-endif()
-
-set(CPACK_PACKAGE_NAME cryptoutils)
-set(CPACK_PACKAGE_VENDOR arxflay)
-set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-set(CPACK_PACKAGE_VERSION_MAJOR ${VER_MAJOR})
-set(CPACK_PACKAGE_VERSION_MINOR ${VER_MINOR})
-set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${PKG_VER_NAME}-${CPACK_SYSTEM_NAME}")
-if(WIN32)
-    set(CPACK_GENERATOR NSIS)
-else()
-    set(CPACK_GENERATOR TGZ)
+    set(PKG_VER_NAME "${PKG_VER_NAME}(${COMMIT_ID})")
 endif()
--- libs/ProjectLibs.cmake
+++ libs/ProjectLibs.cmake
@@ -1,9 +1,9 @@
-include(${CMAKE_CURRENT_LIST_DIR}/FindFmt.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/FindFmt.cmake)
 
 if (NOT ${LIBFMT_FOUND})
     set(CPM_LOCAL_PACKAGES_ONLY False)
     set(CPM_SOURCE_CACHE ${CMAKE_CURRENT_LIST_DIR}/third_party/CPM)
-    include(${CMAKE_CURRENT_LIST_DIR}/FetchCPM.cmake)
-    include(${CMAKE_CURRENT_LIST_DIR}/cmake/CPM.cmake)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/FetchCPM.cmake)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CPM/CPM.cmake)
     CPMAddPackage("gh:fmtlib/fmt#7.1.3")
 endif()
