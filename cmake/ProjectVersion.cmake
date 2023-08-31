4ea95108135bc408a3d27575a29b39580281493d
arxflay <arxflay@yandex.ru>
2023-08-31 23:46:30 +0200

    Fix version display in ProjectVersion

--- cmake/ProjectVersion.cmake
+++ cmake/ProjectVersion.cmake
@@ -1,21 +1,22 @@
 find_package(Git REQUIRED)
-execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} describe --abbrev=0
-    RESULT_VARIABLE GIT_PROJ_TAG ERROR_QUIET)
+execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} describe --candidates=0 --abbrev=0 --tags 
+    OUTPUT_VARIABLE GIT_PROJ_TAG  ECHO_OUTPUT_VARIABLE)
 
-string(REGEX MATCH "^v([0-9]+.)+[0-9]$" VERSION_TAG "${GIT_PROJ_TAG}")
+string(REGEX MATCH "v([0-9]+.)+[0-9]" VERSION_TAG "${GIT_PROJ_TAG}")
 
 if (NOT VERSION_TAG)
     execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} rev-parse --short HEAD
         OUTPUT_VARIABLE COMMIT_ID ERROR_QUIET)
     string(STRIP ${COMMIT_ID} COMMIT_ID)
-    execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} describe --abbrev=0 --tags
+    execute_process(COMMAND git -C ${CMAKE_CURRENT_SOURCE_DIR} describe --abbrev=0 --tags 
         OUTPUT_VARIABLE GIT_PROJ_TAG ERROR_QUIET)
-    string(REGEX MATCH "^v([0-9]+.)+[0-9]$" VERSION_TAG "${GIT_PROJ_TAG}")
+    string(REGEX MATCH "v([0-9]+.)+[0-9]" VERSION_TAG "${GIT_PROJ_TAG}")
 endif()
 
 if (VERSION_TAG)
     string(REGEX MATCH "[0-9]+" VER_MAJOR "${VERSION_TAG}")
-    string(REGEX MATCH "(?=v[0-9]+.)[0-9]+" VER_MINOR "${VERSION_TAG}")
+    string(REGEX MATCH "\\.[0-9]+" VER_MINOR "${VERSION_TAG}")
+    string(REGEX MATCH "[0-9]+" VER_MINOR "${VER_MINOR}")
     set(PKG_VER_NAME ${VERSION_TAG})
 else()
     if (NOT COMMIT_ID)
